f70eeef4a7478faaadd670e2a42c4c98d94b3686
hschneider <h_schneider@marketmix.com>
2026-01-02 22:41:29 +0100

    Update

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center" style="margin-top: -100px">
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
