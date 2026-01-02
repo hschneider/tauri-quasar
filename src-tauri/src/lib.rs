811a66c9eb28278a6dfdc3dca59ab2433dd736df
hschneider <h_schneider@marketmix.com>
2026-01-02 16:50:31 +0100

    Update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,15 +3,15 @@ use std::fs;
 use tauri::Manager;
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn ping() -> String {
+    format!("PONG from the Rust Backend!")
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         //.plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![ping])
 
         .setup(|app| {
             // Delete app cache on startup
@@ -30,6 +30,7 @@ pub fn run() {
             Ok(())
         })
 
+        .plugin(tauri_plugin_dialog::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -4,8 +4,9 @@
     <h2>Subheadline</h2>
     <p>{{ lorem }}</p>
     <p>{{ lorem }}</p>
+    <q-btn class="btn-md" label="Ping the Rust Backend" color="primary" @click="CallBackend" unelevated/>
 
-    <TestTable />
+    <TestTable style="margin: 15px -15px 15px -15px"/>
 
     <q-card dark bordered class="bg-grey-3" style="color: #333; max-width: 300px">
       <q-card-section>
@@ -24,8 +25,17 @@
 
 <script setup>
 import TestTable from 'components/TestTable.vue'
+import { message } from '@tauri-apps/plugin-dialog'
+import { invoke } from '@tauri-apps/api/core'
 
 const lorem =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 
+async function CallBackend() {
+  let res = await invoke('ping')
+  await message(res, {
+    title: 'Greeting',
+    kind: 'info',
+  })
+}
 </script>
